d5cef9c06cd4b675c70ab5f2fb3d9b496a3ed2f3
Jeroen van den Hout <jlvandenhout@gmail.com>
2022-04-19 00:11:19 +0200

    Remove unused entries

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -1,33 +1,29 @@
 const path = require('path');
 
 module.exports = {
-    title: 'EVM Tutorial',
-    url: '/',
-    baseUrl: '/',
-    themes: ['@docusaurus/theme-classic'],
-    plugins: [
-        [
-            '@iota-wiki/plugin-tutorial',
-            {
-                title: 'EVM Tutorial',
-                description:
-                  'In this tutorial, you will learn how to use the EVM in IOTA.',
-                preview: 'evm-tutorial.png',
-                route: 'evm-tutorial/intro',
-                source: 'https://github.com/dr-electron/tutorial-template',
-                tags: ['text'],
-            },
-        ],
-        [
-            '@docusaurus/plugin-content-docs',
-            {
-                id: 'evm-tutorial',
-                path: path.resolve(__dirname, './docs'),
-                routeBasePath: 'evm-tutorial',
-                sidebarPath: path.resolve(__dirname, './sidebars.js'),
-                editUrl: 'https://github.com/Dr-Electron/tutorial-template/edit/main/',
-            },
-        ],
+  plugins: [
+    [
+      '@iota-wiki/plugin-tutorial',
+      {
+        title: 'EVM Tutorial',
+        description:
+          'In this tutorial, you will learn how to use the EVM in IOTA.',
+        preview: 'evm-tutorial.png',
+        route: 'evm-tutorial/intro',
+        source: 'https://github.com/dr-electron/tutorial-template',
+        tags: ['text'],
+      },
     ],
-    staticDirectories: [path.resolve(__dirname, './images')],
+    [
+      '@docusaurus/plugin-content-docs',
+      {
+        id: 'evm-tutorial',
+        path: path.resolve(__dirname, './docs'),
+        routeBasePath: 'evm-tutorial',
+        sidebarPath: path.resolve(__dirname, './sidebars.js'),
+        editUrl: 'https://github.com/Dr-Electron/tutorial-template/edit/main/',
+      },
+    ],
+  ],
+  staticDirectories: [path.resolve(__dirname, './images')],
 };
